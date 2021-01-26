4e9b44309c3188df5848a23033e145c0c1e1af1b
Ying-Chen Cheng <yingchen@Ying-ChendeMacBook-Pro.local>
2021-01-26 21:42:13 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+const { Chromeless } = require('chromeless')
+var startTime;
+var endTime;
+
+// add all URLs you want to visit here
+const urls = [];
+
+async function run() {
+    const chromeless = new Chromeless()
+    startTime = Date.now();
+    for (var i = 0; i < 10; i++) {
+    const screenshot = await chromeless
+        .goto(urls[i])
+        // .screenshot()
+    }
+    await chromeless.end()
+    endTime = Date.now();
+    console.log((endTime - startTime)/1000 + "s");
+}
+
+run().catch(console.error.bind(console));
